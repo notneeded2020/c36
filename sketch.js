d6a1b741ecaae811ffd1bd37914664a0d2eac75e
notneeded2020 <75374552+notneeded2020@users.noreply.github.com>
2021-03-27 11:57:11 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,27 @@
+var gameState = 0 , database;
+var playerCount = 0;
+var form,game,player;
+
+
+
+function setup(){
+  database = firebase.database();
+  console.log(database);
+  createCanvas(500,500);
+  
+  game = new Game ();
+  game.getState();
+  game.start();
+  
+  
+}
+
+function draw(){
+  background("white");
+  
+  
+    drawSprites();
+  
+}
+
+
